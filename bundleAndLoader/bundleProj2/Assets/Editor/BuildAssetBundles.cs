a2e5fd9b26059bef6f6c36e416fae51b1e766069
matrix3d <sliz@qq.com>
2019-04-12 14:48:23 +0800

    bundle and loader

---
+++ bundleAndLoader/bundleProj2/Assets/Editor/BuildAssetBundles.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildAssetBundles  {
+	[MenuItem("my/BuildAllBundles")]
+	static void BuildAllBundles()
+	{
+		//找到所有prefab 并设置导出名字
+		List<string> paths = new List<string>();
+		doFiles(Application.dataPath, paths);
+		//Dictionary<string, GameObject> path2gameobject = new Dictionary<string, GameObject>();
+		foreach (string path in paths)
+		{
+			string rpath = path.Replace(Application.dataPath, "Assets");
+			AssetImporter asset = AssetImporter.GetAtPath(rpath);
+			asset.assetBundleName = rpath;
+			//GameObject go = (GameObject)AssetDatabase.LoadAssetAtPath(rpath, typeof(GameObject));
+			//path2gameobject.Add(rpath, go);
+			Debug.Log(path);
+			Debug.Log(rpath);
+		}
+		string dir = "../AssetBundles";
+		if (!Directory.Exists(dir))
+		{
+			Directory.CreateDirectory(dir);
+		}
+		BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+	}
+
+	private static void doFiles(string path, List<string> arr)
+	{
+		if (File.Exists(path))
+		{
+			if (Path.GetExtension(path) == ".prefab")
+			{
+				arr.Add(path);
+			}
+		}
+		else if (Directory.Exists(path))
+		{
+			foreach (string str in Directory.GetFileSystemEntries(path))
+			{
+				doFiles(str, arr);
+			}
+		}
+	}
+}
---
+++ bundleAndLoader/loaderProj/Assets/script/LoadModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LoadModel : MonoBehaviour {
+	string dir = "../AssetBundles/";
+	private Object go;
+	IEnumerator Start () {
+		yield return loadAsset("assets/models/model1.prefab");
+		Instantiate(go);
+		yield return loadAsset("assets/models/model2.prefab");
+		Instantiate(go);
+	}
+
+	IEnumerator loadAsset(string url)
+	{
+		string path= Application.absoluteURL + dir + url;
+		path=Path.GetFullPath(path);
+		System.UriBuilder uriBuilder = new System.UriBuilder(path);
+		uriBuilder.Scheme = "file";
+		Debug.Log(uriBuilder.ToString());
+		WWW www = new WWW(uriBuilder.ToString());//AssetBundle.LoadFromFileAsync("F:/New Unity Project (2)/Assets/AssetBundles/thirdpc");
+		yield return www;
+		AssetBundle ab = www.assetBundle;
+		if (ab == null)
+		{
+			Debug.Log("加载失败");
+		}
+		go = ab.LoadAsset(url);
+	}
+}

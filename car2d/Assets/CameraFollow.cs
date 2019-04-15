a0f6ce13eff93cbf5f8b156be161b39cdb5e3bdb
matrix3d <sliz@qq.com>
2019-04-15 14:39:10 +0800

    car2d\Assembly-CSharp.csproj

---
+++ car2d/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject target;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var fp = base.transform.position;
+		var tp = target.transform.position;
+		fp.x += (tp.x - fp.x) * .9f;
+		fp.y += (tp.y - fp.y) * .9f;
+		base.transform.position = fp;
+	}
+}
---
+++ car2d/Assets/move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+	public WheelJoint2D leftWheelJoint;
+	public WheelJoint2D rightWheelJoint;
+	public float speed = 500;
+	public float maxSpeed = 10000;
+	public float rotSpeed = 1000;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var h = Input.GetAxis("Horizontal");
+		leftWheelJoint.useMotor = h != 0;
+		rightWheelJoint.useMotor = h != 0;
+		var mt = leftWheelJoint.motor;
+		mt.motorSpeed = -speed * h;
+		mt.maxMotorTorque = maxSpeed;
+		leftWheelJoint.motor = mt;
+		mt = rightWheelJoint.motor;
+		mt.motorSpeed = -speed * h;
+		mt.maxMotorTorque = maxSpeed;
+		rightWheelJoint.motor = mt;
+		var v = Input.GetAxis("Vertical")*Mathf.Abs(h);
+		GetComponent<Rigidbody2D>().AddTorque(-v * rotSpeed);
+	}
+}

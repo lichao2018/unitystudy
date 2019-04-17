ddb06a609bd17958df3b2476ef04c4a7d4d0e743
matrix3d <sliz@qq.com>
2019-04-17 16:00:14 +0800

    ik\Assembly-CSharp.csproj

---
+++ ik/Assets/CtrlTip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlTip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log((new Vector3(2, 2, 3)).sqrMagnitude);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.transform.Translate(.01f, 0, 0);
+	}
+}
